c42cd822c1ca03afe9d93e1d32cd7f44e0dda36f
Chandan U <chandanucodeid04@gmail.com>
2024-10-01 14:32:50 +0530

    Update 0031-next-permutation.cpp

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,3 +1,37 @@
+// Brute
+class Solution {
+private:
+    void recursionPermutaion(vector<int>& nums, vector<int>& carry,vector<vector<int>>& ans,vector<bool>& used){
+        if(carry.size()==nums.size()){
+            ans.push_back(carry);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(used[i]) continue;
+            carry.push_back(nums[i]);
+            used[i]=true;
+            recursionPermutaion(nums,carry,ans,used);
+            carry.pop_back();
+            used[i]=false;
+        }
+    }
+public:
+    void nextPermutation(vector<int>& nums) {
+        vector<int> carry;
+        vector<vector<int>> ans;
+        vector<bool> used(nums.size(),false);
+        recursionPermutaion(nums,carry,ans,used);
+        sort(ans.begin(),ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        auto it = find(ans.begin(),ans.end(),nums);
+        if(it!=ans.end()){
+            if((it+1)==ans.end()) nums = ans[0];
+            else nums = *(it+1);
+        }
+    }
+};
+
+// Optimal
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
