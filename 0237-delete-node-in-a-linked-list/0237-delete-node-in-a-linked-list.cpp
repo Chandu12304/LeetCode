92e7bff2d51f4cb2c0db9368d429c7a8f176e438
Chandan U <chandanucodeid04@gmail.com>
2024-10-02 22:59:21 +0530

    Update 0237-delete-node-in-a-linked-list.cpp

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -6,10 +6,26 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// Brute O(n)
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-       node->val = node->next->val;
-    node->next= node->next->next;
+        ListNode *ptr= node->next;
+        while(ptr->next!=NULL){
+            node->val = ptr->val;
+            node = node->next;
+            ptr = ptr->next;
+        }
+        node->val = ptr->val;
+        node->next=NULL;
+    }
+};
+
+// Optimal O(1)
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next= node->next->next;
     }
 };
