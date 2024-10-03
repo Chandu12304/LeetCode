61c9d45975201151f0dffb8674c44721f3d79ef8
Chandan U <chandanucodeid04@gmail.com>
2024-10-03 23:48:18 +0530

    Update 0142-linked-list-cycle-ii.cpp

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -27,3 +27,32 @@ public:
         return s;
     }
 };
+
+// Count of the nodes in cycle
+class Solution {
+  public:
+    // Function to find the length of a loop in the linked list.
+    int countNodesinLoop(Node *head) {
+        // Code here
+        int cnt=1;
+        if(head==NULL) return 0;
+        ListNode *s= head;
+        ListNode *f=head;
+        while(f!=NULL and f->next!=NULL){
+            f=f->next->next;
+            s=s->next;
+            if(f==s) break;
+        }
+        if(f==NULL or f->next==NULL) return 0;
+        s=head;
+        while(s!=f){
+            s=s->next;
+            f=f->next;
+        }
+        f=f->next;
+        while(f!=s){
+            f=f->next;
+            cnt+=1;
+        }
+        return cnt;
+    }
