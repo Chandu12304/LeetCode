06da3231831cdfeaf28865b27e3ce3e1ee9e40a2
Chandan U <chandanucodeid04@gmail.com>
2024-10-05 19:34:14 +0530

    Time: 3 ms (41.84%), Space: 8 MB (42.08%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,35 +5,31 @@ public:
         int l = haystack.size();
 
         // Edge cases
-        if (s == 0) return 0;
-        if (s > l) return -1;
+        if (s == 0) return 0; // If needle is empty
+        if (s > l) return -1; // If needle is longer than haystack
 
-        // Bad character heuristic map (rightmost position of characters in needle)
-        map<char, int> mp;
-        for (int i = 0; i < s; i++) {
-            mp[needle[i]] = i;
+        // Create a bad character shift table
+        vector<int> shift(256, s); // Default shift to the size of needle
+        for (int i = 0; i < s - 1; i++) { // Populate shift table for needle characters
+            shift[needle[i]] = s - 1 - i; // Store distance from the end
         }
 
-        int j = 0; // Pointer in haystack
-        while (j <= l - s) {
-            int i = s - 1; // Pointer in needle
-
-            // Compare needle with current substring in haystack
+        int j = 0; // Pointer for haystack
+        while (j <= l - s) { // While there's still enough room for needle in haystack
+            int i = s - 1; // Start comparison from the end of needle
+            
+            // Compare needle with the current substring in haystack
             while (i >= 0 && needle[i] == haystack[j + i]) {
                 i--;
             }
 
             // If needle is found
-            if (i < 0) return j;
+            if (i < 0) return j; // Match found at index j
 
-            // Use bad character heuristic to determine jump length
-            if (mp.find(haystack[j + i]) != mp.end()) {
-                j += max(1, i - mp[haystack[j + i]]);
-            } else {
-                j += i + 1;
-            }
+            // Use the shift table for jumping ahead
+            j += shift[haystack[j + s - 1]]; // Shift based on the last character in current substring
         }
-        
-        return -1;
+
+        return -1; // No match found
     }
 };
