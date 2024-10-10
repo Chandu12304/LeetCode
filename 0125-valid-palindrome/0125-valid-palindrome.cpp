b29f6636072576b62611823b57706f7f7975d14f
Chandan U <chandanucodeid04@gmail.com>
2024-10-11 00:30:01 +0530

    Time: 9 ms (22.24%), Space: 9.4 MB (29.73%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string filtered;
+        for (char c : s) {
+            if (isalnum(c)) {
+                filtered += tolower(c);
+            }
+        }
+        return equal(filtered.begin(), filtered.end(), filtered.rbegin());
+    }
+};
