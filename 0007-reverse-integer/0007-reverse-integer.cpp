ff7806c0c22c0876e58da754031946c60dd94866
Chandan U <chandanucodeid04@gmail.com>
2024-10-10 21:27:40 +0530

    Time: 5 ms (20.38%), Space: 8.3 MB (44.48%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,7 +5,7 @@ public:
         
         while (x != 0) {
             int digit = x % 10; 
-            
+            // A 32-bit signed integer ranges from -2^31 to 2^31 - 1 (i.e., from -2147483648 to 2147483647).
             if (reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 && digit > 7)) return 0;  
             if (reversed < INT_MIN / 10 || (reversed == INT_MIN / 10 && digit < -8)) return 0;
             
