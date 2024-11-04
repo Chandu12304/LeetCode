9a9e579ad3015be438b70e92f886ba1614587f83
Chandan U <chandanucodeid04@gmail.com>
2024-11-04 19:54:05 +0530

    Time: 0 ms (100%), Space: 12.4 MB (57.04%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -2,6 +2,7 @@ class Solution {
 
 public:
     int findKthPositive(vector<int>& arr, int k) {
+        // Brute (linear) :- O(n)
         int val=k;
         for(int x: arr){
             if(val<x) return val;
